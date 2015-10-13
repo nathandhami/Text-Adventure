9789dd4b1e296895f8299c3fa16f1c7070cde512
Nathan Dhami <nda24@sfu.ca>
2015-10-12 22:29:21 -0700

    Refactored and cleaned up code

--- yamlparser/yamlparser.cpp
+++ yamlparser/yamlparser.cpp
@@ -10,13 +10,14 @@
 #include<vector>
 #include "yaml-cpp/yaml.h"
 
-YAML::Node config;
+
 
 void loadYamlCppFile(std::string fileName) {
 //	 config = YAML::LoadFile(fileName);
 //	 std::ofstream fout(fileName);
 //	 fout << config;
 }
+
 // int zoneID, string zoneName, string description,
 //string extendedDesc, int northID, string northDesc, int southID,
 //string southDesc, int eastID, string eastDesc, int westID, string westDesc,
@@ -39,149 +40,135 @@ struct ParseRoom {
 	std::string downDesc;
 };
 
-void parseRooms(const YAML::Node config){
+void parseRooms(const YAML::Node config) {
 
+	if (config["ROOMS"]) {
+
+		YAML::Node roomNodes = config["ROOMS"];
+		int numOfRooms = 0;
+
+		for (YAML::iterator it = roomNodes.begin(); it != roomNodes.end();
+				++it) {
+
+			const YAML::Node roomNode = *it;
+
+
+			std::cout << "Room # " << ++numOfRooms << std::endl;
+			std::cout << "------------------------" << std::endl;
+
+			std::cout << "Room Description: " << std::endl;
+			for (unsigned int i = 0; i < roomNode["desc"].size(); ++i) {
+				std::cout << roomNode["desc"][i] << std::endl;
+			}
+
+			for (unsigned int j = 0; j < roomNode["doors"].size(); ++j) {
+				std::cout << "Door #" << j + 1 << std::endl;
+				std::cout << "---------------" << std::endl;
+				std::cout << "Door Description: " << std::endl;
+				for (unsigned int k = 0;
+						k < roomNode["doors"][j]["desc"].size(); ++k)
+					std::cout << roomNode["doors"][j]["desc"][k] << std::endl;
+				std::cout << "dir : " << roomNode["doors"][j]["dir"] << std::endl;
+				std::cout << "keywords: " << std::endl;
+				for (unsigned int k = 0;
+						k < roomNode["doors"][j]["keywords"].size(); ++k)
+					std::cout << roomNode["doors"][j]["keywords"][k]
+							<< std::endl;
+				std::cout << "to: " << roomNode["doors"][j]["to"] << std::endl;
+			}
+
+			for (unsigned int j = 0;
+					j < roomNode["extended_descriptions"].size(); ++j) {
+				for (unsigned int k = 0;
+						k < roomNode["extended_descriptions"][j]["desc"].size();
+						++k)
+					std::cout << roomNode["extended_descriptions"][j]["desc"][k]
+							<< std::endl;
+
+				// if checks
+				for (unsigned int k = 0;
+						k
+								< roomNode["extended_descriptions"][j]["keywords"].size();
+						++k)
+					std::cout
+							<< roomNode["extended_descriptions"][j]["keywords"][k]
+							<< std::endl;
+			}
+			std::cout << "---------------" << std::endl;
+			std::cout << "room name: " << roomNode["name"] << std::endl;
+			std::cout << "room id: " << roomNode["id"] << std::endl;
+		}
+
+	}
 }
 
-void addRoomsToDatabase(){
+void addRoomsToDatabase() {
 }
 
-int main() {
-//	loadYamlCppFile("midgaard.yml");
+std::string trimString(std::string string) {
 
-	YAML::Node config = YAML::LoadFile("midgaard.yml");
+	return string;
+}
 
-//	YAML::Node node = YAML::Load("id: 3000");
-//	assert(node.IsMap());
-//
-//	YAML::Node node1 = YAML::Load("NPCS:");
-//		assert(node1.IsMap());
-//
-//	YAML::Node node2 = YAML::Load("description:");
-//				assert(node2.IsMap());
-
-	std::string str;
+void parseArea(const YAML::Node& config) {
 	if (config["AREA"]) {
+
+		std::string str;
 		if (config["AREA"]["name"]) {
-
 			str = config["AREA"]["name"].as<std::string>();
-
 			std::cout << "It does exist " << str << std::endl;
 		} else {
 			std::cout << "It does not exist" << std::endl;
 		}
-
 	} else {
 		std::cout << "area does not exist" << std::endl;
 	}
-//////////////////////////////////////////////////////////////////////
-//	int str2;
+}
+
+void parseNPC(const YAML::Node& config) {
+
 	YAML::Node npcNodes = config["NPCS"];
 
-	if (config["NPCS"][0]["description"]) {
-
-		for (YAML::iterator it2 = npcNodes.begin(); it2 != npcNodes.end();
-				++it2) {
-			const YAML::Node npcNode = *it2;
-
-			for (unsigned int i = 0; i < npcNode["keywords"].size(); ++i) {
-				std::cout << npcNode["keywords"][i] << std::endl;
-			}
-		}
-
-//		std::cout << config["NPCS"][0]["description"][0].as<std::string>() << std::endl;
-//		std::cout << config["NPCS"][0]["description"][1].as<std::string>() << std::endl;
-//		std::cout << config["NPCS"][0]["description"][2].as<std::string>() << std::endl;
-//		std::cout << "# of string nodes: " << config["NPCS"][0]["description"].size() << std::endl;
-	}
-
-	// NPC PARSE
 	int numOfNPCs = 0;
 	if (config["NPCS"]) {
 		std::cout << "NPC DOES EXIST" << std::endl;
-
 		for (YAML::iterator it = npcNodes.begin(); it != npcNodes.end(); ++it) {
-
 			const YAML::Node npcNode = *it;
 			std::cout << "NPC # " << ++numOfNPCs << std::endl;
-
 			std::cout << "--------------------------------------------"
 					<< std::endl;
-
 			std::cout << "Description: " << std::endl;
 			for (unsigned int i = 0; i < npcNode["description"].size(); ++i) {
 				std::cout << npcNode["description"][i] << std::endl;
 			}
-
 			std::cout << "id: " << npcNode["id"].as<int>() << std::endl;
-
-			std::cout <<"keywords: " << std::endl;
+			std::cout << "keywords: " << std::endl;
 			for (unsigned int i = 0; i < npcNode["keywords"].size(); ++i) {
 				std::cout << npcNode["keywords"][i] << std::endl;
 			}
-			std::cout <<"longdesc :" << std::endl;
+			std::cout << "longdesc :" << std::endl;
 			for (unsigned int i = 0; i < npcNode["longdesc"].size(); ++i) {
 				std::cout << npcNode["longdesc"][i] << std::endl;
 			}
-//				 for(YAML::iterator it2 = npcNode["description"]; npcNode)
-//				 std::cout << "long desc: " << npcNode[2]["keywords"][0].as<std::string>() << std::endl;
+
 			std::cout << "short desc: "
 					<< npcNode["shortdesc"].as<std::string>() << std::endl;
-//				str2 = config["NPCS"]["description"]["id"].as<int>();
-//
-//				std::cout << "It does exist " << str2 << std::endl;
-//			}
-//			else{
-//				std::cout << "It does not exist" << std::endl;
-//			}
+
 			std::cout << "--------------------------------------------"
 					<< std::endl;
 		}
-
 	} else {
 		std::cout << "NPC does not exist" << std::endl;
 	}
+}
 
-	YAML::Node root = config["NPCS"];
-	YAML::Node node = root["NPCS"];
+int main() {
 
-	// We now have a map node, so let's iterate through:
-	for (YAML::iterator it = node.begin(); it != node.end(); ++it) {
-		YAML::Node key = it->first;
-		YAML::Node value = it->second;
-		if (key.Type() == YAML::NodeType::Sequence) {
-			// This should be true; do something here with the scalar key.
-			std::cout << "It is scalar" << std::endl;
-		}
-		if (value.Type() == YAML::NodeType::Map) {
-			// This should be true; do something here with the map.
-			std::cout << "It is a map" << std::endl;
-		}
-	}
+	YAML::Node config = YAML::LoadFile("midgaard.yml");
 
-	YAML::Node descriptionNode = config["description"];
-
-//	YAML::Node parseDescription = YAML::Load("")
-
-	YAML::Node primes = YAML::Load("{-rofl,-lmao}");
-	if (primes.IsSequence()) {
-		std::cout << "WOOT" << std::endl;
-	}
-	// or:
-//	for (YAML::const_iterator it=primes.begin();it!=primes.end();++it) {
-//		 std::cout << "Playing at " << it->first.as<std::string>() << "\n";
-//	}
-
-	if (descriptionNode.IsMap()) {
-		std::cout << "It is a map" << std::endl;
-	} else if (descriptionNode.IsScalar()) {
-		std::cout << "It is a scalar" << std::endl;
-	} else if (descriptionNode.IsSequence()) {
-		std::cout << "It is a sequence" << std::endl;
-	} else {
-		std::cout << "It is a string" << std::endl;
-
-	}
+//	parseArea(config);
+//	parseNPC(config);
+	parseRooms(config);
 
 	return 0;
 }
