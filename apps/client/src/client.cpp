af0eb1a465179648de9f2fb431a682af410a3063
Gener Iglesias <giglesia@sfu.ca>
2015-10-30 12:46:40 -0700

    Updated UI with server response textbox and user command textbox

--- apps/client/src/client.cpp
+++ apps/client/src/client.cpp
@@ -65,29 +65,43 @@ int main( int argc, const char* argv[] ) {
 	Gtk::Entry commandEntry;
 	Gtk::Window window;
 	Gtk::Button button("Click Me");
-	Gtk::Box commandBox(Gtk::ORIENTATION_HORIZONTAL, 5);
+	Gtk::Box outputBox;
 	Gtk::Frame commandFrame("Enter Command");
+	Gtk::Frame outputFrame("Server");
+
+	outputTextview.set_editable(FALSE);
 	
+	//Initialize main window
 	window.set_default_size(900, 500);
 	window.set_title("Text Gale Online");
 	window.set_position(Gtk::WIN_POS_CENTER);
 	window.set_border_width(10);
 
+	//Adds the output text to scrollwindow parent
 	outputScrollWindow.add(outputTextview);
 
-	//window.add(button);
+	//This expands the scrollwindow vertically
+	outputScrollWindow.set_vexpand(true);
+
+	commandEntry.set_width_chars(85);
+	outputBox.set_border_width(5);
+
+	outputBox.pack_start(outputScrollWindow);
+
+	//Adds the entry box child to its own frame
+	commandFrame.add(commandEntry);
+	//Adds the output box child to its own frame
+	outputFrame.add(outputBox);
+
 	uiGrid.set_orientation(Gtk::ORIENTATION_VERTICAL);
+	uiGrid.add(outputFrame);
 	uiGrid.add(commandFrame);
-	uiGrid.add(commandEntry);
 
-//	commandFrame.add(uiGrid);
-
+	//Adds the grid to the main window
 	window.add(uiGrid);
 	
 	window.show_all_children();
-	//button.show();
-	//window.add(button);
-	
+
 	//kit.run(window);
 	Gtk::Main::run(window);
 	/*UIWriter::sendSysStatus( "Connecting to server..." );
